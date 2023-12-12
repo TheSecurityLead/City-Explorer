9ded230186693684401b739ea5f82a75894538b3
Shama-David Grier-Johnson <127712361+TheSecurityLead@users.noreply.github.com>
2023-12-12 23:16:25 +0300

    Update CityExplorer.jsx

--- src/CityExplorer.jsx
+++ src/CityExplorer.jsx
@@ -21,7 +21,7 @@ const CityExplorer = () => {
       const regionURL = cityIsInUS(city) ? 'https://us1.locationiq.com/v1/search.php' : 'https://eu1.locationiq.com/v1/search.php';
       const response = await axios.get(regionURL, {
         params: {
-          key: process.env.REACT_APP_CITY_EXPLORER,
+          key: 'pk.d412df6ffd1d3aaabcda549b559fe485',
           q: city,
           format: 'json'
         }
